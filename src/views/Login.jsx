7a5a05bf8bb1130e02546889be948c6d75fe8891
kacpro9 <sterkowiczkacper@gmail.com>
2025-09-23 07:55:45 +0200

    add login& signup

---
+++ src/views/Login.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+
+const Login = ({ user, setUser }) => {
+  const [formaData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loginMessage, setLoginMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formaData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:3005/auth/login", {
+        email: formaData.email,
+        password: formaData.password,
+      });
+
+      if (res.data.token) {
+        setLoginMessage("Login successful!");
+
+        setUser({ email: formaData.email, token: res.data.token });
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ email: formaData.email, token: res.data.token })
+        );
+      } else {
+        setLoginMessage("Login failed. Please try again.");
+      }
+    } catch (error) {
+      setLoginMessage("An error occurred. Please try again.");
+      console.error("Login error:", error);
+    }
+  };
+
+  return (
+    <div className="login">
+      {user && <Navigate to="/" />}
+      <form onSubmit={handleSubmit}>
+        {loginMessage && <h2>{loginMessage}</h2>}
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button className="btn">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/views/SignUp.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
+
+const SignUp = (props) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [signUpMessage, setSignUpMessage] = useState("");
+
+  const [signUpDone, setSignUpDone] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const validate = () => {
+    let naeErrors = {};
+
+    if (!formData.email.includes("@")) {
+      nameErrors.email = "Invalid email address";
+    }
+  };
+
+  if (formData.password.length < 6) {
+    nameErrors.password = "Password must be at least 6 characters";
+  }
+
+  setErrors(newErrors);
+  return Object.keys(newErrors).length === 0;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validate()) return;
+    try {
+      const res = await axios.post("http://localhost:3005/auth/signup", {
+        email: formData.email,
+        password: formData.password,
+      });
+
+      if (res.status === 201) {
+        setSignUpMessage("Registration successful! You can now log in.");
+        setSignUpDone(true);
+      }
+    } catch (error) {
+      console.error("Sign up error:", error);
+      error.response?.data?.message ||
+        "Error creating account. Please try again.";
+    }
+  };
+
+  return (
+    <div className="signup">
+      {props.user && <Navigate to="/" />}
+      <form onSubmit={handleSubmit}>
+        {signUpMessage && <h2>{signUpMessage}</h2>}
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        {errors.email && <p className="error">{errors.email}</p>}
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        {errors.password && <p className="error">{errors.password}</p>}
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+        {errors.confirmPassword && (
+          <p className="error">{errors.confirmPassword}</p>
+        )}
+        <button type="submit" disabled={signUpDone}>
+          {signUpDone ? "Registered" : "Sign Up"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
