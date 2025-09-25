66fb13e859ed8ea3337c522c7d81f2fee4d94727
kacpro9 <sterkowiczkacper@gmail.com>
2025-09-25 09:18:26 +0200

    change

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ function App() {
           <Link to="/clients/new" className="nav-link">
             Add Client
           </Link>
+          <Link to="/signup" className="nav-link">
+            Sign Up
+          </Link>
+          <Link to="/login" className="nav-link">Login</Link>
         </div>
       </nav>
 
